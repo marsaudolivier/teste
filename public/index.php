43755134e3c13465e06706e87e6491716fb9a6d3
Marsaud olivier <cougies79700@gmail.com>
2023-11-21 11:16:53 +0100

    maj deploye

--- public/index.php
+++ public/index.php
@@ -7,3 +7,8 @@ require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 return function (array $context) {
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
+Request::setTrustedProxies(
+    // trust *all* requests
+    ['127.0.0.1', $request->server->get('REMOTE_ADDR')],
+    Request::HEADER_X_FORWARDED_ALL
+);
